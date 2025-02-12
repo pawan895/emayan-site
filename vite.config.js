715c04f9e2a7d5b55917065e73fd8d1b25b7d464
pawan895 <bpawan2002@gmail.com>
2025-02-12 23:42:34 +0530

    changes

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,7 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react"; // Import the React plugin
+
 export default defineConfig({
-  plugins: [tailwindcss(), React()],
+  plugins: [tailwindcss(), react()], // Use the React plugin correctly
 });
